26042aa50bec02d2ac6b59bb31bfadd1dbe137be
liyinlong <liyinlong>
2022-03-05 12:57:07 +0800

    fix-修复添加失败的bug

--- src/main/java/com/example/ldapdemo/SecurityConfig.java
+++ src/main/java/com/example/ldapdemo/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.example.ldapdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
-import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
@@ -13,30 +11,19 @@ import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.server.UnboundIdContainer;
 import org.springframework.security.ldap.userdetails.PersonContextMapper;
 
-import java.util.List;
-
 @Configuration
 public class SecurityConfig {
+    @Bean
+    UnboundIdContainer ldapContainer() {
+        UnboundIdContainer container = new UnboundIdContainer("dc=springframework,dc=org", "classpath:users.ldif");
+        container.setPort(0);
+        return container;
+    }
 
-//    @Bean
-//    LdapTemplate ldapTemplate() {
-//        LdapContextSource contextSource = new LdapContextSource();
-//        contextSource.setUrl("ldap://10.10.101.140:1389");
-//        contextSource.setUserDn("cn=admin,dc=springframework,dc=org");
-//        contextSource.setPassword("Hc@Cloud01");
-//        try {
-//            contextSource.afterPropertiesSet();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//
-//        LdapTemplate ldapTemplate = new LdapTemplate(contextSource);
-//        List<String> list = ldapTemplate.list("ou=cloudnative,dc=springframework,dc=org");
-//        list.forEach(item -> {
-//            System.out.println(item);
-//        });
-//        return ldapTemplate;
-//    }
+    @Bean
+    ContextSource contextSource(UnboundIdContainer container) {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:" + container.getPort() + "/dc=springframework,dc=org");
+    }
 
     @Bean
     BindAuthenticator authenticator(BaseLdapPathContextSource contextSource) {
